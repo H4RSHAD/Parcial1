74c9f6af2b5be215cfe299913d71e97e7c0052d4
Franz <franzribera12@gmail.com>
2025-07-05 02:21:05 -0400

    inicio1

---
+++ parcial1/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from config import BaseConfig
+from flask_migrate import Migrate
+import os
+
+
+
+
+
+app = Flask(__name__,static_folder = BaseConfig.STATIC_FOLDER, template_folder = BaseConfig.TEMPLATE_FOLDER)
+
+app.config.from_object('config.DevConfig')  # traigo las configuraciones de DevConfig
+
+
+
+from .models.User import db       # importo el db para poder migrar a la base de datos
+
+migrate = Migrate(app, db)                  # realiza las migraciones
+
+from .routers import home
+
+app.register_blueprint(home, url_prefix="/")
